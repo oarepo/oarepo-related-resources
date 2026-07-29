ac4e2adc0c41432defa9dc612032556279de2683
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-07-29 12:15:08 +0200

    fix: lint

--- tests/conftest.py
+++ tests/conftest.py
@@ -119,8 +119,7 @@ def crossref_imported_metadata():
             }
         ],
         "publication_date": "1940",
-        'publisher': 'Massachusetts Institute of Technology and Woods Hole '
-                     'Oceanographic Institution',
+        "publisher": "Massachusetts Institute of Technology and Woods Hole Oceanographic Institution",
         "resource_type": {"id": "dataset"},
         "title": "Observations of vertical humidity distribution above the ocean surface and their relation to "
         "evaporation",
